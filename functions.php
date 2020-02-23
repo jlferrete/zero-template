7e6ba327321e843287dd29e1309e577064159040
jlferrete <joseluisferrete@gmail.com>
2020-02-23 11:32:00 +0100

    Basic Pagination

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Theme functions
+ * 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy
+ * 
+ * @package Zero
+ */
+ ?>
+
+<?php
+
+if ( function_exists( 'add_theme_support' ) )
+add_theme_support( 'post-thumbnails' );
+
+
+?>
